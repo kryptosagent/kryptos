e62bc0e46801bae348db092ec5b70ee780c43b3a
kryptosagent <hello@kryptosagent.xyz>
2025-12-09 06:45:44 +0700

    fix: force webpack mdoe for vercel deployment

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  turbopack: {},
   transpilePackages: ['@privy-io/react-auth'],
   webpack: (config, { isServer }) => {
     if (!isServer) {
